8930c188e3152cfb94e05a9c0d40b4138a9e557e
Vivek Singh <viveksingh.pat5@gmail.com>
2023-05-04 18:41:57 +0530

    DataTypes_basic

---
+++ src/com/java/basics/datatypes/DataTypeMinMax.java
@@ -0,0 +1,25 @@
+package com.java.basics.datatypes;
+
+public class DataTypeMinMax {
+
+    public static void main(String[] args) {
+
+        System.out.println(Byte.MAX_VALUE + " ------  " + Byte.MAX_VALUE);
+        System.out.println(Short.MAX_VALUE + " ------  " + Short.MAX_VALUE);
+        System.out.println(Long.MAX_VALUE + " ------  " + Long.MAX_VALUE);
+        System.out.println(Integer.MAX_VALUE + " ------  " + Integer.MAX_VALUE);
+        System.out.println(Double.MAX_VALUE + " ------  " + Double.MAX_VALUE);
+        System.out.println(Character.MAX_VALUE + " ------  " + Character.MAX_VALUE);
+        //System.out.println(Boolean.MAX_VALUE+" ------  "+Boolean.MAX_VALUE); // compilation err
+/*
+
+127 ------  127
+32767 ------  32767
+9223372036854775807 ------  9223372036854775807
+2147483647 ------  2147483647
+1.7976931348623157E308 ------  1.7976931348623157E308
+￿ ------  ￿
+ */
+
+    }
+}
---
+++ src/com/java/basics/datatypes/ImplicitTypeCasting.java
@@ -0,0 +1,10 @@
+package com.java.basics.datatypes;
+
+public class ImplicitTypeCasting {
+
+    public static void main(String[] args) {
+        byte b = 10;
+        int i = b;
+        System.out.println(b + "  " + i); // 10  10
+    }
+}
