276769f71add06d2e9f867ef76071c196176974a
Vivek Singh <viveksingh.pat5@gmail.com>
2023-05-04 18:52:11 +0530

    DataTypes_basic_updated

--- src/com/java/basics/datatypes/DataTypeMinMax.java
+++ src/com/java/basics/datatypes/DataTypeMinMax.java
@@ -4,7 +4,7 @@ public class DataTypeMinMax {
 
     public static void main(String[] args) {
 
-        System.out.println(Byte.MAX_VALUE + " ------  " + Byte.MAX_VALUE);
+        System.out.println(Byte.MAX_VALUE + " -------  " + Byte.MAX_VALUE);
         System.out.println(Short.MAX_VALUE + " ------  " + Short.MAX_VALUE);
         System.out.println(Long.MAX_VALUE + " ------  " + Long.MAX_VALUE);
         System.out.println(Integer.MAX_VALUE + " ------  " + Integer.MAX_VALUE);
